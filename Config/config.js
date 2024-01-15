342130c60f868583f33df135014bb7e9f9f32ab0
ak220193 <mail2meak22frcrio@gmail.com>
2024-01-15 19:22:13 +0530

    ENV files updated with Mongo url

--- Config/config.js
+++ Config/config.js
@@ -1,15 +1,17 @@
 import morgan from "morgan";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const logger = morgan("combined");
 
 const mongoUrl = process.env.MONGO_URL;
 
-//mongodb+srv://wildbadger:jkhinpiqosq@clusterbankapp.pn7rge3.mongodb.net/?retryWrites=true&w=majority
-
-// 
+// mongodb+srv://wildbadger:jkhinpiqosq@clusterbankapp.pn7rge3.mongodb.net/?retryWrites=true&w=majority;
 
 // const mongoUrl = `mongodb+srv://${process.env.DB_userName}:${process.env.DB_password}@academy.ilmon77.mongodb.net/${process.env.DB_name}?retryWrites=true&w=majority`;
+
+// ("mongodb+srv://wildbadger:jkhinpiqosq@clusterbankapp.pn7rge3.mongodb.net/?retryWrites=true&w=majority");
+
+// MONGO_URL = mongodb://learnplusplus_admin:oCmxyOOjm@103.180.120.237:27017/learnplusplus_data
 
 export { logger, mongoUrl };
--- index.js
+++ index.js
@@ -4,35 +4,32 @@ import mongoose from "mongoose";
 import routes from "./Routes/Routes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = Express();
 
 const PORT = process.env.PORT || 8001;
 app.use(cors());
-// app.use(cors({ credentials: true, origin: process.env.BASE_URL ?? "http://localhost:3000"})); //https://llp-nk6wy77ku-versatilemage.vercel.app, http://learnplusplus.com
-//https://learnplusplus-pi.vercel.app https://learnplusphase1.vercel.app https://learnplus-phase1.vercel.app/  https://llp-hazel.vercel.app
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api", routes);
 
 mongoose.connect(
   mongoUrl,
   {
     useNewUrlParser: true,
-    useUnifiedtopology: true,
+    useUnifiedTopology: true, // Fix the typo here
   },
   (err) => {
-    if (!err) {
-      console.log("connected to db");
+    if (err) {
+      console.error("Error connecting to MongoDB:", err);
     } else {
-      console.log("error", err);
+      console.log("Connected to MongoDB");
     }
   }
 );
 
 app.listen(PORT, () => {
-  console.log(`server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
